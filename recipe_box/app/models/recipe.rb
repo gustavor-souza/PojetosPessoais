43f02d25af38eebfee4f40816cfd6e52b52e7f20
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-05 23:17:58 -0300

    Finlizado recipe box

--- recipe_box/app/models/recipe.rb
+++ recipe_box/app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   has_many :directions
+  belongs_to :user
 
   accepts_nested_attributes_for :ingredients,
                               reject_if: proc { |attributes| attributes['name'].blank? },
@@ -8,7 +9,7 @@ class Recipe < ActiveRecord::Base
   accepts_nested_attributes_for :directions,
                               reject_if: proc { |attributes| attributes['step'].blank? },
                               allow_destroy: true
-  
+
   validates :title, :description, :image, presence: true
 
   has_attached_file :image, styles: { medium: "400x300>" }
