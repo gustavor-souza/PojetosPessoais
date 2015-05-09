1a991551adcc319b1ff4899aa3c35778e1de15d7
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-09 02:12:45 -0300

    Finalizadeo projeto

--- pin_board/app/controllers/pins_controller.rb
+++ pin_board/app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
-  before_action :find_pin, only: [:show,:edit,:update,:destroy]
-
+  before_action :find_pin, only: [:show,:edit,:update,:destroy,:upvote]
+  before_action :authenticate_user!, except: [:index,:show] 
   def index
     @pins = Pin.all.order('created_at DESC')
   end
@@ -21,6 +21,11 @@ class PinsController < ApplicationController
   end
 
   def edit
+  end
+
+  def upvote
+    @pin.upvote_by current_user
+    redirect_to :back
   end
 
   def update
