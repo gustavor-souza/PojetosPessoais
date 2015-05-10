c9ba883fdd1ad80cc3c9c766035e9754a677e115
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-10 15:58:08 -0300

    projeto finalizado

--- todo_list/app/controllers/todo_items_controller.rb
+++ todo_list/app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo
-
+  before_action :set_todo_item, except: [:create]
+  
   def create
     @todo_item = @todo.todo_items.create(todo_item_params)
     redirect_to @todo
@@ -16,7 +17,20 @@ class TodoItemsController < ApplicationController
     redirect_to @todo
   end
 
+  def complete
+    if @todo_item.completed_at.blank?
+      @todo_item.update_attribute(:completed_at, Time.zone.now)
+    else
+      @todo_item.update_attribute(:completed_at, nil)
+    end
+    redirect_to @todo, notice: "Item completo"
+  end
+
   private
+  def set_todo_item
+    @todo_item = TodoItem.find(params[:id])
+  end
+
   def set_todo
     @todo = Todo.find(params[:todo_id])
   end
