f31b3b1bbc910dd8eb5b9ef85d403e2b7910fe36
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-18 23:26:22 -0300

    adicionado devise

---
+++ wiki/app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show]
+
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:content)
+  end
+
+end
