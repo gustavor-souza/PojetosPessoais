3bb3e497483bd46c490c30027757a2944f3d56b8
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-19 20:24:26 -0300

    Finalizado prokjeto wiki

--- wiki/app/controllers/articles_controller.rb
+++ wiki/app/controllers/articles_controller.rb
@@ -1,19 +1,24 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only:[:show]
+  before_action :set_article, only:[:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, except:[:index, :show]
 
   def index
-    @articles = Article.all.order('created_at DESC')
+    if params[:category_id].blank?
+      @articles = Article.all.order('created_at DESC')
+    else
+      @articles = Article.where(category_id: params[:category_id]).order('created_at DESC')
+    end
   end
 
   def show
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       redirect_to @article
     else
@@ -21,10 +26,20 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def update
+  def edit
   end
 
-  def edit
+  def update
+    if @article.update_attributes(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path, notice: "Article successfuly deleted"
   end
 
   private
@@ -34,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title,:content)
+    params.require(:article).permit(:title,:content,:category_id)
   end
 
 end
--- wiki/db/schema.rb
+++ wiki/db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519022356) do
+ActiveRecord::Schema.define(version: 20150519024917) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+  end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
