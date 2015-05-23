dc289702bde8703c24b5e0986e09ef4cc9e1a12e
Gustavo Souza <gustavo_bjj@hotmail.com>
2015-05-22 23:09:51 -0300

    Primeiro commit

--- notenote/config/routes.rb
+++ notenote/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
   get 'welcome/index'
+
+  authenticated :user do
+	  root "notes#index", as: "authenticated_root"
+	end
+  
+  root 'welcome#index'
 
   resources :notes
 end
